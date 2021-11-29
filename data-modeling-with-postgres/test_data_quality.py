d29638ed6b860f6ddec7a825c4f31e369f2d1211
zkan <zkan.cs@gmail.com>
2021-11-29 22:26:15 +0700

    Move Postgres connection to fixture

--- data-modeling-with-postgres/test_data_quality.py
+++ data-modeling-with-postgres/test_data_quality.py
@@ -1,30 +1,36 @@
 import psycopg2
+import pytest
 
 
-def test_it_should_be_seventy_one_rows_in_song_where_artist_id_is_not_null():
+@pytest.fixture
+def potgres_connection():
     conn = psycopg2.connect(
         "host=127.0.0.1 dbname=sparkifydb user=student password=student"
     )
     cur = conn.cursor()
+
+    return conn, cur
+
+
+def test_it_should_be_seventy_one_rows_in_song_where_artist_id_is_not_null(
+    potgres_connection,
+):
+    conn, cur = potgres_connection
 
     select = """
         SELECT * FROM songs WHERE artist_id IS NOT NULL;
     """
     cur.execute(select)
     results = cur.fetchall()
+    conn.close()
 
     assert len(results) == 71
 
-    print("✅ There are 71 rows where artist ID is not null")
 
-    conn.close()
-
-
-def test_it_should_be_one_row_where_both_song_id_and_artist_id_are_not_null():
-    conn = psycopg2.connect(
-        "host=127.0.0.1 dbname=sparkifydb user=student password=student"
-    )
-    cur = conn.cursor()
+def test_it_should_be_one_row_where_both_song_id_and_artist_id_are_not_null(
+    potgres_connection,
+):
+    conn, cur = potgres_connection
 
     select = """
         SELECT
@@ -35,13 +41,10 @@ def test_it_should_be_one_row_where_both_song_id_and_artist_id_are_not_null():
     """
     cur.execute(select)
     results = cur.fetchall()
+    conn.close()
 
     assert len(results) == 1
 
     song_id, artist_id = results[0]
     assert song_id == "SOZCTXZ12AB0182364"
     assert artist_id == "AR5KOSW1187FB35FF4"
-
-    print("✅ There is 1 row with values for value containing ID for both song ID and artist ID")
-
-    conn.close()
