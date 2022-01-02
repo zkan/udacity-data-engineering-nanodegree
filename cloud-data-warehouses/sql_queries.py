21922f72ad733279efdf3d5268c95553e8203703
zkan <zkan.cs@gmail.com>
2022-01-02 09:22:16 +0700

    Write drop statements + write statements to create fact and dimention tables

--- cloud-data-warehouses/sql_queries.py
+++ cloud-data-warehouses/sql_queries.py
@@ -7,35 +7,80 @@ config.read("dwh.cfg")
 
 # DROP TABLES
 
-staging_events_table_drop = ""
-staging_songs_table_drop = ""
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+staging_events_table_drop = "DROP TABLE IF EXISTS staging_events"
+staging_songs_table_drop = "DROP TABLE IF EXISTS staging_songs"
+songplay_table_drop = "DROP TABLE IF EXISTS songplays"
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
 staging_events_table_create = """
+    CREATE TABLE IF NOT EXISTS staging_events (
+    )
 """
 
 staging_songs_table_create = """
+    CREATE TABLE IF NOT EXISTS staging_songs (
+    )
 """
 
 songplay_table_create = """
+    CREATE TABLE IF NOT EXISTS songplays (
+        songplay_id serial primary key,
+        start_time timestamp not null,
+        user_id int not null,
+        level varchar,
+        song_id varchar,
+        artist_id varchar,
+        session_id int,
+        location varchar,
+        user_agent varchar
+    )
 """
 
 user_table_create = """
+    CREATE TABLE IF NOT EXISTS users (
+        user_id int primary key,
+        first_name varchar not null,
+        last_name varchar not null,
+        gender varchar,
+        level varchar
+    )
 """
 
 song_table_create = """
+    CREATE TABLE IF NOT EXISTS songs (
+        song_id varchar primary key,
+        title varchar not null,
+        artist_id varchar not null,
+        year int,
+        duration numeric
+    )
 """
 
 artist_table_create = """
+    CREATE TABLE IF NOT EXISTS artists (
+        artist_id varchar primary key,
+        name varchar not null,
+        location varchar,
+        latitude numeric,
+        longitude numeric
+    )
 """
 
 time_table_create = """
+    CREATE TABLE IF NOT EXISTS time (
+        start_time timestamp primary key,
+        hour int,
+        day int,
+        week int,
+        month int,
+        year int,
+        weekday int
+    )
 """
 
 # STAGING TABLES
