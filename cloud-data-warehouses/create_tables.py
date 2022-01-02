c9c9805d427aaa96cb4a14cea184230a4a247b4f
zkan <zkan.cs@gmail.com>
2022-01-02 08:50:46 +0700

    Format code with Black

--- cloud-data-warehouses/create_tables.py
+++ cloud-data-warehouses/create_tables.py
@@ -1,5 +1,7 @@
 import configparser
+
 import psycopg2
+
 from sql_queries import create_table_queries, drop_table_queries
 
 
@@ -17,9 +19,13 @@ def create_tables(cur, conn):
 
 def main():
     config = configparser.ConfigParser()
-    config.read('dwh.cfg')
+    config.read("dwh.cfg")
 
-    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+    conn = psycopg2.connect(
+        "host={} dbname={} user={} password={} port={}".format(
+            *config["CLUSTER"].values()
+        )
+    )
     cur = conn.cursor()
 
     drop_tables(cur, conn)
--- cloud-data-warehouses/etl.py
+++ cloud-data-warehouses/etl.py
@@ -17,11 +17,15 @@ def insert_tables(cur, conn):
 
 def main():
     config = configparser.ConfigParser()
-    config.read('dwh.cfg')
+    config.read("dwh.cfg")
 
-    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+    conn = psycopg2.connect(
+        "host={} dbname={} user={} password={} port={}".format(
+            *config["CLUSTER"].values()
+        )
+    )
     cur = conn.cursor()
-    
+
     load_staging_tables(cur, conn)
     insert_tables(cur, conn)
 
