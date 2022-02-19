901ea62424088986bd00180169bd10dcc4de6155
zkan <zkan.cs@gmail.com>
2022-02-19 16:31:58 +0700

    Remove unused code

--- data-pipelines-with-airflow/mnt/dags/song_play_etl_pipeline.py
+++ data-pipelines-with-airflow/mnt/dags/song_play_etl_pipeline.py
@@ -13,8 +13,6 @@ from operators import (
 )
 from helpers import SqlQueries
 
-# AWS_KEY = os.environ.get('AWS_KEY')
-# AWS_SECRET = os.environ.get('AWS_SECRET')
 
 default_args = {
     "owner": "Kan Ouivirach",
