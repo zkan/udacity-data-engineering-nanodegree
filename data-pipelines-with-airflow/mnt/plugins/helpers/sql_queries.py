bb3eaf9d1ad94ecc1cb0e70b9b7717737c3896b7
zkan <zkan.cs@gmail.com>
2022-02-19 15:55:08 +0700

    Adjust format

--- data-pipelines-with-airflow/mnt/plugins/helpers/sql_queries.py
+++ data-pipelines-with-airflow/mnt/plugins/helpers/sql_queries.py
@@ -1,22 +1,24 @@
 class SqlQueries:
     songplay_table_insert = """
         SELECT
-                md5(events.sessionid || events.start_time) songplay_id,
-                events.start_time,
-                events.userid,
-                events.level,
-                songs.song_id,
-                songs.artist_id,
-                events.sessionid,
-                events.location,
-                events.useragent
-                FROM (SELECT TIMESTAMP 'epoch' + ts/1000 * interval '1 second' AS start_time, *
+            md5(events.sessionid || events.start_time) songplay_id,
+            events.start_time,
+            events.userid,
+            events.level,
+            songs.song_id,
+            songs.artist_id,
+            events.sessionid,
+            events.location,
+            events.useragent
+        FROM (
+            SELECT TIMESTAMP 'epoch' + ts/1000 * interval '1 second' AS start_time, *
             FROM staging_events
-            WHERE page='NextSong') events
-            LEFT JOIN staging_songs songs
-            ON events.song = songs.title
-                AND events.artist = songs.artist_name
-                AND events.length = songs.duration
+            WHERE page='NextSong'
+        ) events
+        LEFT JOIN staging_songs songs
+        ON events.song = songs.title
+            AND events.artist = songs.artist_name
+            AND events.length = songs.duration
     """
 
     user_table_insert = """
