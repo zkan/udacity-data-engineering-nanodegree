ec8ec488f6b0ccf5182ab80ab7dfe34bf6336a5d
zkan <zkan.cs@gmail.com>
2022-02-19 13:05:09 +0700

    Set task dependencies

--- data-pipelines-with-airflow/mnt/dags/song_play_etl_pipeline.py
+++ data-pipelines-with-airflow/mnt/dags/song_play_etl_pipeline.py
@@ -59,3 +59,5 @@ with DAG(
     run_quality_checks = DataQualityOperator(task_id="Run_data_quality_checks")
 
     end_operator = DummyOperator(task_id="Stop_execution")
+
+    start_operator >> [stage_events_to_redshift, stage_songs_to_redshift] >> load_songplays_table >> [load_user_dimension_table, load_song_dimension_table, load_artist_dimension_table, load_time_dimension_table] >> run_quality_checks >> end_operator
