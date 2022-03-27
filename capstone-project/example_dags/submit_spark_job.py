980f40a1a05d7bcfec17d0bbf9914db9c85a5df9
zkan <zkan.cs@gmail.com>
2022-03-27 13:26:20 +0700

    Add example DAG that submits Spark job

---
+++ capstone-project/example_dags/submit_spark_job.py
@@ -0,0 +1,21 @@
+from airflow import DAG
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+from airflow.utils import timezone
+
+
+default_args = {
+    "owner": "Kan Ouivirach",
+    "start_date": timezone.datetime(2022, 3, 20),
+}
+with DAG(
+    "submit_spark_job",
+    default_args=default_args,
+    schedule_interval=None
+) as dag:
+
+    data_processing = SparkSubmitOperator(
+        task_id="data_processing",
+        conn_id="spark_conn",
+        application="/usr/local/spark/app/data_processing.py",
+        verbose=False
+    )
